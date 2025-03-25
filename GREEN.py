622cad64390f5332f9eac8edee4fc6244b8905a2
MOHAMED-143 <158847209+MOHAMEED-143@users.noreply.github.com>
2025-03-26 00:02:21 +0100

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -1,1 +1,15 @@
-import L64
+import os, platform, time, sys
+os.system('clear')
+os.system('git pull')
+ranaxd = platform.architecture()[0]
+os.system('clear')
+if ranaxd == '64bit':
+ print(' [+] Your Device is 64bit')
+ time.sleep(1)
+ import M64
+elif ranaxd == '32bit':
+ print(' [+] Your Devive is 32bit')
+ time.sleep(1)
+ import M64
+ 
+
