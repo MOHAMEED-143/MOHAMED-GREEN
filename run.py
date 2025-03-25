d65e8f51c8ff02b5cb079806163cab7850b4c8d8
MOHAMED-143 <158847209+MOHAMEED-143@users.noreply.github.com>
2025-03-25 21:00:07 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,12 @@
+import os, platform, time, sys
+
+ranaxd = platform.architecture()[0]
+if ranaxd == '64bit':
+ print(' [+] Your Device is 64bit')
+ time.sleep(1)
+ import M64
+elif ranaxd == '32bit':
+ print(' [+] Your Devive is 32bit')
+ time.sleep(1)
+ import M64
+ 
