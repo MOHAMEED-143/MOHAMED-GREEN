4834214572258e81d65eb44c4de64157234dd702
MOHAMED-143 <158847209+MOHAMEED-143@users.noreply.github.com>
2025-03-26 00:02:43 +0100

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -6,10 +6,10 @@ os.system('clear')
 if ranaxd == '64bit':
  print(' [+] Your Device is 64bit')
  time.sleep(1)
- import M64
+ import MO7AMED
 elif ranaxd == '32bit':
  print(' [+] Your Devive is 32bit')
  time.sleep(1)
- import M64
+ import MO7AMED
  
 
